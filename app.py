78786812268994e80723352b66130b5211cb7330
naman-kr <70480613+naman-kr@users.noreply.github.com>
2020-09-16 12:45:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request,jsonify
+lask import Flask, render_template, request, jsonify
 from flask_cors import CORS,cross_origin
 import requests
 from bs4 import BeautifulSoup as bs
@@ -6,12 +6,12 @@ from urllib.request import urlopen as uReq
 
 app = Flask(__name__)
 
-@app.route('/',methods=['GET'])
+@app.route('/',methods=['GET'])  # route to display the home page
 @cross_origin()
 def homePage():
     return render_template("index.html")
 
-@app.route('/review',methods=['POST','GET'])
+@app.route('/review',methods=['POST','GET']) # route to show the review comments in a web UI
 @cross_origin()
 def index():
     if request.method == 'POST':
@@ -29,6 +29,7 @@ def index():
             prodRes = requests.get(productLink)
             prodRes.encoding='utf-8'
             prod_html = bs(prodRes.text, "html.parser")
+            #print(prod_html)
             commentboxes = prod_html.find_all('div', {'class': "_3nrCtb"})
 
             filename = searchString + ".csv"
@@ -38,12 +39,14 @@ def index():
             reviews = []
             for commentbox in commentboxes:
                 try:
+                    #name.encode(encoding='utf-8')
                     name = commentbox.div.div.find_all('p', {'class': '_3LYOAd _3sxSiS'})[0].text
 
                 except:
                     name = 'No Name'
 
                 try:
+                    #rating.encode(encoding='utf-8')
                     rating = commentbox.div.div.div.div.text
 
 
@@ -51,12 +54,14 @@ def index():
                     rating = 'No Rating'
 
                 try:
+                    #commentHead.encode(encoding='utf-8')
                     commentHead = commentbox.div.div.div.p.text
 
                 except:
                     commentHead = 'No Comment Heading'
                 try:
                     comtag = commentbox.div.div.find_all('div', {'class': ''})
+
                     custComment = comtag[0].div.text
                 except Exception as e:
                     print("Exception while creating dictionary: ",e)
@@ -69,11 +74,11 @@ def index():
         except Exception as e:
             print('The Exception message is: ',e)
             return 'something is wrong'
+    # return render_template('results.html')
 
     else:
         return render_template('index.html')
 
 if __name__ == "__main__":
+    #app.run(host='127.0.0.1', port=8001, debug=True)
 	app.run(debug=True)
-
-
